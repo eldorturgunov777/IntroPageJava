9a340bf242e45f3a3400c9af626e68798f9481fd
eldorturgunov777 <eldorturgunov777@gmail.com>
2022-05-12 11:34:50 +0500

    latest

--- app/src/main/java/com/example/intropagejava/MainActivity.java
+++ app/src/main/java/com/example/intropagejava/MainActivity.java
@@ -58,6 +58,7 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
 
+
             @Override
             public void onTabUnselected(TabLayout.Tab tab) {
             }
